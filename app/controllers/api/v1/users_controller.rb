b04bca8200270960824e93a05b8023f5da21c175
Jason Gomez <jason.gomez@me.com>
2019-07-24 13:31:15 -0400

    Fix image_url so that it gets an actual url and not a stringified object.
    Signed-off-by: Jason Gomez <jason.gomez@me.com>

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,10 +4,9 @@ class Api::V1::UsersController < ApplicationController
     auth_params = Spotify.new.login(params[:spotifyUriCode])
     user_data = Spotify.new.grab_user_data(auth_params['access_token'])
     user = User.find_or_create_by(user_params(user_data))
-    img_url = user_data['images'][0] ? user_data['images'][0] : nil
+    img_url = user_data['images'][0] ? user_data['images'][0]['url'] : nil
     encoded_access_token = encode_token(token: auth_params['access_token'])
     encoded_refresh_token = encode_token(token: auth_params['refresh_token'])
-
     user.update(profile_img_url: img_url, access_token: encoded_access_token, refresh_token: encoded_refresh_token)
 
     render json: user.to_json(except: %i[access_token refresh_token created_at updated_at])
